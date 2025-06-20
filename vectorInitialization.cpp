5f9899e73ec21fcb5245ddb9c1b03fc9e294090a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-20 17:44:43 +0600

    Explored vector initialization

---
+++ vectorInitialization.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // vector<int> v(10, -1);
+    // int integerArray[5] = {1, 2, 3, 4, 5};
+
+    vector<int> secondVector = {1, 2, 3, 4, 5};
+    int vectorSize = secondVector.size();
+
+    for (int i = 0; i < vectorSize; i++)
+    {
+        cout << secondVector[i] << " ";
+    }
+
+    return 0;
+}
